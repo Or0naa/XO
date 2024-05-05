c1f06da1f6145a8be6fc997bf86834a8c5a39635
orna <or0naa@gmail.com>
2024-05-05 12:32:35 +0300

    x&O

--- xo_client/src/App.jsx
+++ xo_client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import X_index from './components/XO/X_index'
+import O_index from './components/XO/O_index'
 
 export default function App() {
   return (
     <div>
-      
+  
     </div>
   )
 }
