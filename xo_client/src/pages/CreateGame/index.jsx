9b26ead3a09fcb6122067acd35b6882a6e803c03
eliraz <elish7@gmail.com>
2024-05-12 13:53:27 +0300

    style cahnges

--- xo_client/src/pages/CreateGame/index.jsx
+++ xo_client/src/pages/CreateGame/index.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import useSocket from '../../socket';
 import { useState, useEffect } from 'react';
+import { useGameStore } from '../../store';
 
 export default function CreateGame() {
   const nav = useNavigate();
@@ -18,10 +19,6 @@ export default function CreateGame() {
 
   const [roomNumber, setRoomNumber] = useState(null); // State to store the received room number
 
-  const { game, setGame } = useGameStore(state => ({
-    game: state.game,
-    setGame: state.setGame
-  }));
 
 
   useEffect(() => {
@@ -50,12 +47,38 @@ export default function CreateGame() {
   return (
     <div className={styles.createContainer}>
       <BackArrow />
-      <Frame>
-        <h1>Your Code: {roomNumber}</h1> 
-      </Frame>
+      <div className={styles.title}>
+    <h1>Your Code: </h1>   
+    <div className={styles.roomNumber}>
+        <Frame>
+            {roomNumber}
+        </Frame>
+    </div>
+</div>
       {/* <h1>Send message</h1>
       <a href='https://wa.link/lime1k' target='_blank'>This is your link</a> */}
-      <div className={styles.loader}></div>
+      <svg
+          className={styles.svg}
+          viewBox="0 0 187.3 93.7"
+          height="300px"
+          width="400px"
+        >
+          <path
+            d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z"
+            className={styles.outline} // Change id to className
+            stroke="#ffd414"
+            strokeWidth="5"
+            fill="none"
+          ></path>
+          <path
+            d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z"
+            className={styles.outlineBg} // Change id to className
+            stroke="#ffd414"
+            fill="none"
+            strokeWidth="5"
+            opacity="0.05"
+          ></path>
+        </svg>
       { connect ? 
       <Button>
         <h2 onClick={() => nav('/choose')}>Choose Shape</h2>
